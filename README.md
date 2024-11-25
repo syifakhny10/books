d39285bc8e5b9d6aebb0add2df82db020e6d9a10
syifakhny10 <syifakharisma729@gmail.com>
2024-11-25 15:14:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Praktikum Pemrograman Asynchronous
+Nama: Syifa Kharisma Nayla
+NIM: 362358302019
+Kelas: 2B TRPL
+
+# Praktikum 1: Mengunduh Data dari Web Service (API)
+- Mencari judul buku favorite di google books, lalu ganti ID buku pada variabel path di kode tersebut. Dan kemudian akses di browser URI tersebut dengan lengkap.
+![alt text](image.png)
+
+- Tambah kode di ElevatedButton, lalu lakukan run aplikasi Flutter. Hasilnya seperti gambar di bawah ini.
+![alt text](image-1.png)
+- Substring adalah metode atau fungsi dalam banyak bahasa pemrograman yang digunakan untuk mengambil bagian tertentu dari sebuah string (sub-bagian string). Fungsi ini biasanya menerima parameter awal (index) dan akhir (opsional) untuk menentukan bagian string yang ingin diambil.
+- catchError biasanya merujuk pada mekanisme penanganan error (kesalahan) dalam bahasa pemrograman.
+
+#  Praktikum 2: Menggunakan await/async untuk menghindari callbacks
+![alt text](image-2.png)
+Langkah 1 :
+Kode tersebut mensimulasikan operasi yang membutuhkan waktu tertentu dengan penundaan menggunakan Future.delayed. Hal ini berguna dalam mengelola proses asinkron dalam aplikasi.
+
+Langkah 2 : 
+Kode tersebut adalah fungsi asinkron dalam bahasa Dart yang menghitung total nilai yang diperoleh dari tiga fungsi returnOneAsync, returnTwoAsync, dan returnThreeAsync. Fungsi ini juga memperbarui tampilan (UI) dengan memanfaatkan setState untuk mengubah nilai variabel result.
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+  Future<int> returnOneAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 1;
+}
+
+Future<int> returnTwoAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 2;
+}
+
+Future<int> returnThreeAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 3;
+}
+
+Future count() async {
+  int total = 0;
+  total = await returnOneAsync();
+  total += await returnTwoAsync();
+  total += await returnThreeAsync();
+  setState(() {
+    result = total.toString();
+  });
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+      Future<Response> getData() async{
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/qwVnDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {
+                count();
+                // setState(() {});
+                //   getData()
+                //   .then((value) {
+                //     result = value.body.toString().substring(0, 450);
+                //     setState(() {});
+                //   }).catchError((_){
+                //     result = 'An error onccurred';
+                //     setState(() {});
+                // });
+              },
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
